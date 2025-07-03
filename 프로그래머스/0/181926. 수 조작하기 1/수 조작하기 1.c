df6b3d682aec61dde2ce948a3999891fd5a6e6d6
DongUkKIM21 <162282992+DongUkKIM21@users.noreply.github.com>
2025-07-03 18:02:05 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.70 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int solution(int n, const char* control) {
+    int answer = n;
+
+    for (int i = 0; i < strlen(control); i++) {
+        if (control[i] == 'w') {
+            answer += 1;
+        }
+        else if (control[i] == 's') {
+            answer -= 1;
+        }
+        else if (control[i] == 'd') {
+            answer += 10;
+        }
+        else if (control[i] == 'a') {
+            answer -= 10;
+        }
+    }
+
+    return answer;
+}
