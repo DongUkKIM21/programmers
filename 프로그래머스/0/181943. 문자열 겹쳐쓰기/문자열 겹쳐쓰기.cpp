7f703588b131177881beb26c9c7db25edaf0284a
DongUkKIM21 <162282992+DongUkKIM21@users.noreply.github.com>
2025-07-05 01:59:34 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    
+    answer += my_string.substr(0,s);
+    
+    answer += overwrite_string;
+    
+    if(s + overwrite_string.size() < my_string.size()){
+        answer += my_string.substr(s + overwrite_string.size());
+    }
+    
+    return answer;
+}
